29ba53c60effad484ff94ebf9601958a203debdb
Hariom <hariom2001chaurasia@gmail.com>
2020-10-01 11:40:28 +0530

    Initial creation

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 
+    'mongodb+srv://Hariom:cEiOe3zQQ55ancBO@cluster0-zv18e.mongodb.net/test?authSource=admin&replicaSet=Cluster0-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+}
---
+++ server.js
@@ -0,0 +1,27 @@
+const { decodeBase64 } = require('bcryptjs');
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
+
+
+const app=express();
+const port=5000||process.env.PORT;
+const db = require('./config/keys').mongoURI;
+
+mongoose
+  .connect(db,{ useNewUrlParser: true,useUnifiedTopology: true })
+  .then(()=> console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+app.get('/',(req,res)=>res.send('Hello'));
+
+app.use('/api/users',users);
+app.use('/api/profile',profile);
+app.use('/api/posts',posts);
+
+
+
+app.listen(port,()=> console.log(`Server listening to ${port} port`));
